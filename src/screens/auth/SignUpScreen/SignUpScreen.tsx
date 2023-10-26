86f955c974c7c134d252d426801f0eb81c32e98f
Adalberto Junior <adljunior@rd.com.br>
2023-10-26 15:31:30 -0300

    feat: SignUpScreen with zod and react hook form

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,21 +1,34 @@
-import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { CText } from "../../../components/CText/CText";
-import { CScreen } from "../../../components/CScreen/CScreen";
-import { CTextInput } from "../../../components/CTextInput/CTextInput";
 import { CButton } from "../../../components/CButton/CButton";
-import { CPasswordInput } from "../../../components/CPasswordInput/CPasswordInput";
+import { CFormPasswordInput } from "../../../components/CForm/CFormPasswordInput";
+import { CFormTextInput } from "../../../components/CForm/CFormTextInput";
 
-import { RootStackParamList } from "../../../routes/Routes";
+import { CScreen } from "../../../components/CScreen/CScreen";
+import { CText } from "../../../components/CText/CText";
 import { useResetNavigationSuccess } from "../../../hooks/useResetNavigationSuccess";
+import { RootStackParamList } from "../../../routes/Routes";
+import { signUpSchema, SignUpSchema } from "./signUpSchema";
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, "SignUpScreen">;
 
 export function SignUpScreen({ navigation }: ScreenProps) {
   const { reset } = useResetNavigationSuccess();
-
-  const submitForm = () => {
+  const { control, formState, handleSubmit } = useForm<SignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      fullName: "",
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+  function submitForm(formValues: SignUpSchema) {
+    console.log(formValues);
     reset({
       title: "Sua conta foi criada com sucesso!",
       description: "Agora é só fazer login na nossa plataforma",
@@ -24,39 +37,50 @@ export function SignUpScreen({ navigation }: ScreenProps) {
         color: "success",
       },
     });
-  };
-
+  }
   return (
     <CScreen canGoBack isScroll>
-      <CText preset="headingLarge" presetFont="bold" mb="s32">
+      <CText preset="headingLarge" mb="s32">
         Criar uma conta
       </CText>
 
-      <CTextInput
+      <CFormTextInput
+        control={control}
+        name="username"
         label="Seu username"
         placeholder="@"
         boxProps={{ mb: "s20" }}
       />
 
-      <CTextInput
-        label="Nome completo"
+      <CFormTextInput
+        control={control}
+        name="fullName"
+        autoCapitalize="words"
+        label="Nome Completo"
         placeholder="Digite seu nome completo"
         boxProps={{ mb: "s20" }}
       />
-
-      <CTextInput
+      <CFormTextInput
+        control={control}
+        name="email"
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{ mb: "s20" }}
       />
 
-      <CPasswordInput
+      <CFormPasswordInput
+        control={control}
+        name="password"
         label="Senha"
         placeholder="Digite sua senha"
-        boxProps={{ mb: "s40" }}
+        boxProps={{ mb: "s48" }}
       />
 
-      <CButton title="Criar minha conta" onPress={submitForm} />
+      <CButton
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+        title="Criar uma conta"
+      />
     </CScreen>
   );
 }
