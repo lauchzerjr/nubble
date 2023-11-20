8a1be7e974561bb3bb65e55384ca6cc7b2f3e32b
Adalberto Junior <adljunior@rd.com.br>
2023-11-20 18:00:54 -0300

    feat: add postComment to domain

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MQ.fbAPp77B3g8kVfrLSwYrPeeCmbAN9wKmqdUVbGqjSJORJ0aqPdslOcLJ_CX9',
+      'Bearer MQ.PlC-SLgnnFWDhnfDdmn4AXX4btY83wAsZJdTg2mnbp-GAmfkwC5CjZNnGOMl',
   },
 });
